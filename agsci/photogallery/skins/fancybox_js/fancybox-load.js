645abae7d243ded699834be9f585ac6ee93c480d
root <root@agplonesrv.ag.psu.edu>
2014-11-06 12:27:17 -0500

    Initial commit

---
+++ agsci/photogallery/skins/fancybox_js/fancybox-load.js
@@ -0,0 +1,45 @@
+jq(document).ready(function() {
+    jq(".contentLeadImageContainer a#parent-fieldname-leadImage, .contentLeadImageContainerLeft a#parent-fieldname-leadImage, .contentLeadImageContainerFullWidth a#parent-fieldname-leadImage").each(
+        function () {
+            var href = jq(this).attr('href');
+            if (href.indexOf('leadImage/image_view_fullscreen') >= 0)
+            {
+                href = href.replace('leadImage/image_view_fullscreen', 'leadImage_galleryzoom');
+                jq(this).attr('href', href);
+                jq(this).addClass('fancybox');
+            }
+            else if (href.indexOf('image_view_fullscreen') >= 0)
+            {
+                href = href.replace('image_view_fullscreen', 'image_galleryzoom');
+                jq(this).attr('href', href);
+                jq(this).addClass('fancybox');
+            }
+        }
+    );
+
+    jq("body.template-atct_album_view #content .photoAlbumImage a").each(
+        function () {
+            var href = jq(this).attr('href');
+            if (href.indexOf('/view') >= 0)
+            {
+                href = href.replace('/view', '/image_galleryzoom');
+                jq(this).attr('href', href);
+                jq(this).addClass('fancybox');
+            }
+        }
+    );
+
+
+    if (window.innerWidth > 520)
+    {
+        jq(".fancybox").fancybox({'type' : 'image', 'hideOnContentClick' : true , 'closeClick' : true});
+    }
+    else
+    {
+        jq(".fancybox").fancybox({'type' : 'image', 'hideOnContentClick' : true , 'closeClick' : true, 'padding' : 0, 'margin' : 2, 'titlePosition' : 'inside'});
+    }
+
+});
+
+
+    
