e51edca878806a912df8e68ce21dfdf2df41514c
tsimkins <trs22@psu.edu>
2015-05-05 10:36:19 -0400

    Updates to photogallery to make more modern and trendy.

--- agsci/photogallery/browser/views.py
+++ agsci/photogallery/browser/views.py
@@ -33,3 +33,29 @@ class PhotoGalleryView(BrowserView):
             # This method handles b_start parametr internally and grabs it from the request object
             return self.context.getFolderContents(contentFilter={'portal_type' : 'Image'}, batch=False, b_size=100)
 
+    def getAspectRatio(self, i, fmt=False):
+        o = i.getObject()        
+
+        if hasattr(o, 'getSize') and hasattr(o.getSize, '__call__'):
+            (w,h) = o.getSize()
+            
+            ratio = float(w)/float(h)
+            
+            if fmt:
+                return '%0.4f' % ratio
+
+            return ratio
+        
+        return 1
+
+
+    def getImageFit(self, i):
+
+        ratio = self.getAspectRatio(i)
+        
+        fit = 'vertical'
+
+        if ratio > 1 and ratio <= 3.0/2:
+            fit = 'horizontal'
+
+        return 'fit-%s' % fit
