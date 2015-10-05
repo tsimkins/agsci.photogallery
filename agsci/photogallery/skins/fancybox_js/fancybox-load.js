87f56267310e36e73296d956b43c2b2781b44885
tsimkins <trs22@psu.edu>
2015-10-05 10:03:57 -0400

    Updated lead image fancybox code to work with changes to leadimage.

--- agsci/photogallery/skins/fancybox_js/fancybox-load.js
+++ agsci/photogallery/skins/fancybox_js/fancybox-load.js
@@ -2,16 +2,9 @@ jq(document).ready(function() {
     jq(".contentLeadImageContainer a#parent-fieldname-leadImage, .contentLeadImageContainerLeft a#parent-fieldname-leadImage, .contentLeadImageContainerFullWidth a#parent-fieldname-leadImage").each(
         function () {
             var href = jq(this).attr('href');
-            if (href.indexOf('leadImage/image_view_fullscreen') >= 0)
+
+            if (href.indexOf('_galleryzoom') >= 0)
             {
-                href = href.replace('leadImage/image_view_fullscreen', 'leadImage_galleryzoom');
-                jq(this).attr('href', href);
-                jq(this).addClass('fancybox');
-            }
-            else if (href.indexOf('image_view_fullscreen') >= 0)
-            {
-                href = href.replace('image_view_fullscreen', 'image_galleryzoom');
-                jq(this).attr('href', href);
                 jq(this).addClass('fancybox');
             }
         }
